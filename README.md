6ad49a498b1ef65436ba86bd7abfd32f7d06d7e0
Ganesh Kumar <mvganeshk15sde@gmail.com>
2020-12-04 11:36:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Morse Code Translater
+
+A translator to convert english to morse code language.
+
+Morse code is used in telecommunication (before it was possible to transmit voice) to encode information as sequences of two different signals called dots and dashes.
+
+Here is the [morse code translator link](https://mvganeshkumar-morse-code-translator.netlify.app "Morse code translator app")
---
+++ app.js
@@ -0,0 +1,53 @@
+const input = document.querySelector("#inputTextarea");
+const btnTranslate = document.querySelector("#btnTranslate");
+const btnReset = document.querySelector("#btnReset");
+const output = document.querySelector("#outputTextarea");
+const alertModal = document.querySelector("#alertModal");
+const audio = document.querySelector("audio");
+
+output.value = "";
+
+btnTranslate.addEventListener("click", () => {
+  output.value = "";
+
+  if (!input.value) {
+    alertModal.style.display = "inline";
+    setTimeout(() => {
+      alertModal.style.display = "none";
+    }, 2000);
+  } else {
+    alertModal.style.display = "none";
+    fetch(
+      `https://api.funtranslations.com/translate/morse.json?text=${input.value}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        output.value = data.contents.translated;
+      })
+      .catch((error) => {
+        console.log(error, error.message);
+      });
+
+    fetch(
+      `https://api.funtranslations.com/translate/morse/audio.json?text=${input.value}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        audio.src = data.contents.translated.audio;
+        console.log(data.contents);
+      })
+      .catch((error) => {
+        console.log(error, error.message);
+      });
+  }
+});
+
+btnReset.addEventListener("click", () => {
+  input.value = "";
+  output.value = "";
+  audio.src = "";
+});
